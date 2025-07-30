5fef3a103d8e16ee70493692b5de8fd13b136e05
Chris McCormick <chrisjmccormick@gmail.com>
2025-07-29 21:49:37 -0700

    Update finetune_sst2.py

--- encoder-pretrain/scripts/finetune_sst2.py
+++ encoder-pretrain/scripts/finetune_sst2.py
@@ -58,7 +58,7 @@ def run_eval(model, dataloader, device):
 def main():
 
     # Checkpoints are saved here.
-    model_path = "/content/shared-subspaces/encoder-pretrain/checkpoints/mla/"
+    model_path = "/content/shared-subspaces/encoder-pretrain/checkpoints/baseline/"
 
     # Confirm directory exists
     assert os.path.exists(model_path), f"Directory does not exist: {model_path}"
