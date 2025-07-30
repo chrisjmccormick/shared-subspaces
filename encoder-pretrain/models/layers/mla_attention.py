7f2336238ed286a05950a63f8dcc2823250799b3
Chris McCormick <chrisjmccormick@gmail.com>
2025-07-30 13:22:45 -0700

    Update mla_attention.py

--- encoder-pretrain/models/layers/mla_attention.py
+++ encoder-pretrain/models/layers/mla_attention.py
@@ -57,6 +57,43 @@ class DeepseekV3RMSNorm(nn.Module):
     def extra_repr(self):
         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
 
+def _compute_default_rope_parameters(
+    config: Optional[PretrainedConfig] = None,
+    device: Optional["torch.device"] = None,
+    seq_len: Optional[int] = None,
+) -> tuple["torch.Tensor", float]:
+    """
+    Computes the inverse frequencies according to the original RoPE implementation
+    Args:
+        config ([`~transformers.PretrainedConfig`]):
+            The model configuration.
+        device (`torch.device`):
+            The device to use for initialization of the inverse frequencies.
+        seq_len (`int`, *optional*):
+            The current sequence length. Unused for this type of RoPE.
+    Returns:
+        Tuple of (`torch.Tensor`, `float`), containing the inverse frequencies for the RoPE embeddings and the
+        post-processing scaling factor applied to the computed cos/sin (unused in this type of RoPE).
+    """
+    base = config.rope_theta
+    partial_rotary_factor = config.partial_rotary_factor if hasattr(config, "partial_rotary_factor") else 1.0
+    
+    # ------------------------------------------------------
+    # Pulled in this function to override this line.
+    # 
+    # head_dim = getattr(config, "head_dim", None) or config.hidden_size // config.num_attention_heads
+    #
+    # Doing so frees us from the constraint that the model size must equal num heads * (rope + qk).
+    # ------------------------------------------------------
+    
+    dim = int(config.qk_rope_head_dim * partial_rotary_factor)
+
+    attention_factor = 1.0  # Unused in this type of RoPE
+
+    # Compute the inverse frequencies
+    inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2, dtype=torch.int64).to(device=device, dtype=torch.float) / dim))
+    return inv_freq, attention_factor
+    
 
 class DeepseekV3RotaryEmbedding(nn.Module):
     def __init__(self, config: SubspaceBertConfig, device=None):
@@ -66,12 +103,18 @@ class DeepseekV3RotaryEmbedding(nn.Module):
             self.rope_type = config.rope_scaling.get("rope_type", config.rope_scaling.get("type"))
         else:
             self.rope_type = "default"
+        
+        assert self.rope_type == "default", "Only default supported"
+        
         self.max_seq_len_cached = config.max_position_embeddings
         self.original_max_seq_len = config.max_position_embeddings
 
         self.config = config
-        self.rope_init_fn = ROPE_INIT_FUNCTIONS[self.rope_type]
-
+        
+        # Overriding to specify rope head size.
+        #self.rope_init_fn = ROPE_INIT_FUNCTIONS[self.rope_type]
+        self.rope_init_fn = _compute_default_rope_parameters
+        
         inv_freq, self.attention_scaling = self.rope_init_fn(self.config, device)
         self.register_buffer("inv_freq", inv_freq, persistent=False)
         self.original_inv_freq = self.inv_freq
